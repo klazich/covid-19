ee56f95ad9961c004eba64e64fcec0fb5b049b68
Kevin <dev@kevinlazich.com>
2020-06-04 15:01:18 -0400

    chore(parser): Move url strings to config file at root

--- src/parser/index.js
+++ src/parser/index.js
@@ -2,20 +2,6 @@ import axios from 'axios'
 import httpAdapter from 'axios/lib/adapters/http'
 import Papa from 'papaparse'
 
-// MongoDB Bulk API stream with mongoose...
-// https://stackoverflow.com/questions/25054958/save-a-very-big-csv-to-mongodb-using-mongoose
-// and...
-// https://stackoverflow.com/questions/37347228/mongodb-mongoose-and-node-js-update-stream
-
-const NYT_DATA_URL =
-  'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-
-const JHU_CSEE_US_TIME_SERIES_URL =
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
-
-const JHU_CSEE_FIPS_LOOKUP_URL =
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv'
-
 async function createHttpStream(url) {
   // Request object for axios
   const request = {
@@ -51,6 +37,4 @@ async function* parseRemoteCSV(url) {
     yield record
   }
 }
-
-export { JHU_CSEE_FIPS_LOOKUP_URL, JHU_CSEE_FIPS_LOOKUP_URL }
 export default parseRemoteCSV
