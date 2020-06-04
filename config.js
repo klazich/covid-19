feb6c55a04ea6f74f4c7ad0fcbd07c8809dd2b3a
Kevin <dev@kevinlazich.com>
2020-06-04 15:01:18 -0400

    chore(parser): Move url strings to config file at root

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  NYT_DATA_URL:
+    'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',
+  JHU_CSEE_US_TIME_SERIES_URL:
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv',
+  JHU_CSEE_FIPS_LOOKUP_URL:
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv',
+}
