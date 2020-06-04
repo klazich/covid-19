c101ba1db203d6efc4fb9159de2d41ce5792979b
Kevin <dev@kevinlazich.com>
2020-06-04 15:07:02 -0400

    feat(database): Add graphql resolvers and update schema

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,7 +1,27 @@
 import { buildSchema } from 'graphql'
 
 export const schema = buildSchema(`
+  type Location {
+    type: String!
+    coordinates: [Float!]!
+  }
+  type Entry {
+    id: ID
+    uid: Int!
+    country_iso2: String!
+    country_iso3: String!
+    country_code: Int!
+    fips: Int!
+    county: String!
+    state: String!
+    country: String!
+    combined_name: String!
+    population: Int!
+    loc: Location!
+    date: String!
+    confirmed: Int!
+  }
   type Query {
-    hello: String
+    listEntries: [Entry]
   }
 `)
