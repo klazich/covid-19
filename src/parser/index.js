eb83a52c03d88eeb3460a0bfd40f0b5546f408fb
Kevin <dev@kevinlazich.com>
2020-06-10 18:58:19 -0400

    refactor(parser): Refactor exported parser function; add helpers

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,5 +1,5 @@
-import stream from './stream'
-import parser from './parser'
+import httpStream from './stream'
+import csvParser from './parser'
 
 export const JHU_CSEE_US_TIME_SERIES_URL =
   'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
@@ -7,11 +7,29 @@ export const JHU_CSEE_US_TIME_SERIES_URL =
 export const JHU_CSEE_FIPS_LOOKUP_URL =
   'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv'
 
-export default async function (url) {
+export async function parser(url) {
   try {
-    const httpStream = await stream(url)
-    return parser(httpStream)
+    const stream = await httpStream(url)
+    return csvParser(stream)
   } catch (error) {
     console.error(error)
+  }
+}
+
+export async function getPopulationMap(url = JHU_CSEE_FIPS_LOOKUP_URL) {
+  const map = new Map()
+  const countryCodes = new Set(['16', '316', '580', '630', '840', '850'])
+  for await (const data of await parser(url)) {
+    if (countryCodes.has(data.code3)) {
+      const { UID, Population } = data
+      map.set(UID, Number(Population))
+    }
+  }
+  return map
+}
+
+export async function* getCovid19Data(url = JHU_CSEE_US_TIME_SERIES_URL) {
+  for await (const data of await parser(url)) {
+    yield data
   }
 }
