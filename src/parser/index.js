2fcacca420fba94cc4a39882d8183368ad4c0560
Kevin <dev@kevinlazich.com>
2020-06-10 10:08:27 -0400

    refactor(parser): :Finish directory refactor

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,6 +1,5 @@
-import axios from 'axios'
-import httpAdapter from 'axios/lib/adapters/http'
-import Papa from 'papaparse'
+import stream from './stream'
+import parser from './parser'
 
 async function createHttpStream(url) {
   // Request object for axios
@@ -11,30 +10,10 @@ async function createHttpStream(url) {
   }
 
   try {
-    const response = await axios(request)
-    return response.data
-  } catch (error) {
-    const { stack } = error.toJSON()
-    console.error(stack ? stack : error)
-  }
-}
-
-async function* parseRemoteCSV(url) {
-  // Parse readable stream instead of file
-  // See: https://github.com/mholt/PapaParse#papa-parse-for-node
-  const csvParserStream = Papa.parse(Papa.NODE_STREAM_INPUT, { header: true })
-
-  try {
-    const httpStream = await createHttpStream(url)
-    // Pipe the http stream to the csv parser
-    httpStream.pipe(csvParserStream)
+    const httpStream = await stream(url)
+    return parser(httpStream)
   } catch (error) {
     console.error(error)
-  }
-
-  // Yield parsed csv data row by row
-  for await (const record of csvParserStream) {
-    yield record
   }
 }
 export default parseRemoteCSV
