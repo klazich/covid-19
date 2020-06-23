ee919acbb896df43f90c5cbed4ccb3a0128e0401
Kevin Lazich <dev@kevinlazich.com>
2020-06-23 08:31:57 -0400

    feat(graphql): Add gql-tools; complete building types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,10 +1,71 @@
+import { join } from 'path'
+
 import { loadSchema } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import {
+  addResolversToSchema,
+  makeExecutableSchema,
+} from '@graphql-tools/schema'
+
+import { resolvers } from './resolvers'
+
+const typeDefs = `
+scalar Date
+
+type Query {
+  entriesByStateAndCounty(state: String!, county: String!): [Entry]
+  entriesByUID(uid: Int!): [Entry]
+  entriesByFIPS(fips: Int!): [Entry]
+  # uids: [Int]
+  counties: [String]
+}
+
+type Entry {
+  id: ID!
+  uid: Int!
+  country_iso2: String!
+  country_iso3: String!
+  country_code: Int!
+  fips: Int!
+  county: String
+  state: String!
+  country: String!
+  combined_name: String!
+  population: Int!
+  loc: Location!
+  date: Date!
+  confirmed: Int!
+}
+
+type Metadata {
+  id: ID!
+  states: [String!]!
+  counties: [String!]!
+  uids: [Int!]!
+  firstDate: String!
+  lastDate: String!
+}
+
+type Location {
+  type: String!
+  coordinates: [Float!]!
+}
+`
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
 
 export async function buildGraphqlSchema() {
-  const schema = await loadSchema('./definitions/queries.graphql', {
-    loaders: [GraphQLFileLoader()],
+  const schema = await loadSchema(join(__dirname, 'types.graphql'), {
+    loaders: [new GraphQLFileLoader()],
   })
 
-  return schema
+  const withResolvers = addResolversToSchema({
+    schema,
+    resolvers,
+  })
+
+  return withResolvers
 }
