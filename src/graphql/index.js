f1ba2026cd3ef4dfdce6fcfda9751032b2585fe4
Kevin <dev@kevinlazich.com>
2020-06-23 18:28:21 -0400

    chore(graphql): Continue working on graphql queries and types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -12,23 +12,34 @@ import { resolvers } from './resolvers'
 const typeDefs = `
 scalar Date
 
+# type Query {
+#   entriesByStateAndCounty(state: String!, county: String!): [Entry]
+#   entriesByUID(uid: Int!): [Entry]
+#   entriesByFIPS(fips: Int!): [Entry]
+#   # uids: [Int]
+#   counties: [String]
+# }
+
 type Query {
-  entriesByStateAndCounty(state: String!, county: String!): [Entry]
-  entriesByUID(uid: Int!): [Entry]
-  entriesByFIPS(fips: Int!): [Entry]
-  # uids: [Int]
-  counties: [String]
+  entries(input: EntriesInput!): [Entry]
+}
+
+input EntriesInput {
+  uid: Int
+  fips: Int
+  state: String
+  county: String
 }
 
 type Entry {
   id: ID!
   uid: Int!
-  country_iso2: String!
+  country_iso2: String
   country_iso3: String!
   country_code: Int!
   fips: Int!
   county: String
-  state: String!
+  state: String
   country: String!
   combined_name: String!
   population: Int!
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -3,15 +3,27 @@ import { DateResolver } from 'graphql-scalars'
 export const resolvers = {
   Date: DateResolver,
   Query: {
-    entriesByStateAndCounty: async (_, { state, county }, { models }) =>
-      await models.Entry.find({ state, county }),
-    entriesByUID: async (_, { uid }, { models }) =>
-      await models.Entry.find({ uid }),
-    entriesByFIPS: async (_, { fips }, { models }) =>
-      await models.Entry.find({ fips }),
-    counties: async (_, {}, { models }) => {
-      const result = await models.Metadata.findOne({}, 'counties')
-      return result.counties
+    // entriesByStateAndCounty: async (_, { state, county }, { models }) =>
+    //   await models.Entry.find({ state, county }),
+    // entriesByUID: async (_, { uid }, { models }) =>
+    //   await models.Entry.find({ uid }),
+    // entriesByFIPS: async (_, { fips }, { models }) =>
+    //   await models.Entry.find({ fips }),
+    // counties: async (_, {}, { models }) => {
+    //   const result = await models.Metadata.findOne({}, 'counties')
+    //   return result.counties
+    entries: async (_, { input }, { models }) => {
+      const { Entry } = models
+      const { uid, fips, state, county } = input
+
+      const test = await Entry.findOne({
+        state: 'Kentucky',
+        county: 'Jefferson',
+      })
+      console.log(test)
+      if (uid) return await Entry.find({ uid })
+      else if (fips) return await Entry.find({ fips })
+      else if (state && county) return await Entry.find({ state, county })
     },
   },
 }
