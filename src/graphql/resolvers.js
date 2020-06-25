06c7a4e4a6dc8757d41f83c84364ce1252b1421c
Kevin <dev@kevinlazich.com>
2020-06-25 17:47:02 -0400

    refactor(graphql): Update graphql schemas and resolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,29 +1,18 @@
-import { DateResolver } from 'graphql-scalars'
+import { DateResolver, ObjectIDResolver } from 'graphql-scalars'
 
 export const resolvers = {
   Date: DateResolver,
+  ObjectID: ObjectIDResolver,
   Query: {
-    // entriesByStateAndCounty: async (_, { state, county }, { models }) =>
-    //   await models.Entry.find({ state, county }),
-    // entriesByUID: async (_, { uid }, { models }) =>
-    //   await models.Entry.find({ uid }),
-    // entriesByFIPS: async (_, { fips }, { models }) =>
-    //   await models.Entry.find({ fips }),
-    // counties: async (_, {}, { models }) => {
-    //   const result = await models.Metadata.findOne({}, 'counties')
-    //   return result.counties
-    entries: async (_, { input }, { models }) => {
+    entries: async (_, { where }, { models }) => {
       const { Entry } = models
-      const { uid, fips, state, county } = input
-
-      const test = await Entry.findOne({
-        state: 'Kentucky',
-        county: 'Jefferson',
-      })
+      console.log(where)
+      const test = await Entry.findOne(where)
       console.log(test)
-      if (uid) return await Entry.find({ uid })
-      else if (fips) return await Entry.find({ fips })
-      else if (state && county) return await Entry.find({ state, county })
+      return await Entry.find(where)
     },
   },
+  // Entry: {
+  //   id: (parent) => parent.__id,
+  // },
 }
