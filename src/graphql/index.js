a062e51fbeccca8b63805ccc48c3deee708c34d8
Kevin Lazich <dev@kevinlazich.com>
2020-06-29 23:23:01 -0400

    feat(graphql): Move typeDefs to own module; add gql schema documentation

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,49 +1,7 @@
-// import { loadSchemaSync } from '@graphql-tools/load'
-// import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
-import {
-  // addResolversToSchema,
-  makeExecutableSchema,
-} from '@graphql-tools/schema'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 
+import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
-
-const typeDefs = `
-scalar Date
-scalar ObjectID
-
-type Query {
-  entries(where: EntriesWhereInput!): [Entry!]
-}
-
-input EntriesWhereInput {
-  uid: Int
-  fips: Int
-  state: String
-  county: String
-}
-
-type Entry {
-  id: ObjectID!
-  uid: Int
-  country_iso2: String
-  country_iso3: String!
-  country_code: Int!
-  fips: Int!
-  county: String
-  state: String
-  country: String!
-  combined_name: String!
-  population: Int!
-  loc: Location!
-  date: Date!
-  confirmed: Int!
-}
-
-type Location {
-  type: String!
-  coordinates: [Float!]!
-}
-`
 
 export const schema = makeExecutableSchema({
   typeDefs,
